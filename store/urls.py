b6efadcb028c9a69a4d1b4b96a96d982f8b27df8
dheereshaggarwal <da11@iitbbs.ac.in>
2022-01-03 12:50:45 +0530

    fixed view

--- store/urls.py
+++ store/urls.py
@@ -5,15 +5,15 @@ from django.conf import settings
 from . import views
 urlpatterns = [
     path('', views.store, name='store'),
-    path('view/<slug:category_or_subcategory_slug>/', views.store,
+    path('category_or_subcategory/<slug:category_or_subcategory_slug>/', views.store,
          name='products_by_category_or_subcategory'),
-    path('view/<slug:category_or_subcategory_slug>/<slug:product_slug>/', views.product_details,
+    path('category_or_subcategory/<slug:category_or_subcategory_slug>/<slug:product_slug>/', views.product_details,
          name='product_details'),
     path('search/', views.search, name='search'),
     path('filter_by_anime/', views.filter_by_anime, name='filter_by_anime'),
     path('submit_review/<int:product_id>/',
          views.submit_review, name='submit_review')
-]
+] 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
